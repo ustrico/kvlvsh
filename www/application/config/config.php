cb5bcb36be9b7913fafdba679ae458a24e6970bc
ustrico <asfom@mail.ru>
2013-07-07 10:00:10 -0700

    Update config.php

--- www/application/config/config.php
+++ www/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://192.168.0.4/';
+$config['base_url']	= 'http://kvlvsh/';
 
 /*
 |--------------------------------------------------------------------------
